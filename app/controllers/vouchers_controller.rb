641041558dc9d2b1a48e4f72aacc4f2ce76c866d
Jen <jenniferanndcb@gmail.com>
2019-09-07 00:46:32 +0100

    vouchers index erb

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -2,6 +2,12 @@ class VouchersController < ApplicationController
 
   get '/vouchers' do 
     @user = User.find(session[:user_id])
+    @user_vouchers = []
+
+    Voucher.all.each do |voucher|
+      @user_vouchers << voucher 
+    end 
+
     erb :'vouchers/index'
     
   end 
@@ -11,8 +17,7 @@ class VouchersController < ApplicationController
   end 
 
   post '/vouchers' do 
-    @voucher = Voucher.create(store_name: params[:store_name], code: params[:code], savings: params[:savings], exp_date: params[:exp_date])
-
+    @vouchers = Voucher.create(store_name: params[:store_name], code: params[:code], savings: params[:savings], exp_date: params[:exp_date])
     redirect '/vouchers'
   end 
 end 
