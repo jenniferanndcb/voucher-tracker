91813214fd9f2583f06e89b9a9d30cc652a4b2a5
Jen <jenniferanndcb@gmail.com>
2019-09-12 13:30:45 +0100

    association between user and voucher working

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -2,11 +2,11 @@ class VouchersController < ApplicationController
 
   get '/vouchers' do 
     @user = User.find(session[:user_id])
-    @user_vouchers = []
+    # @user_vouchers = []
 
-    Voucher.all.each do |voucher|
-      @user_vouchers << voucher 
-    end 
+    # Voucher.all.each do |voucher|
+    #   @user_vouchers << voucher 
+    
 
     erb :'vouchers/index'
     
@@ -44,16 +44,19 @@ class VouchersController < ApplicationController
   end 
 
   patch '/vouchers/:id' do 
-    redirect '/vouchers/id'
+    @voucher = Voucher.find(params[:id])
+    @voucher.store_name = params[:store_name]
+    @voucher.code = params[:code]
+    @voucher.savings = params[:savings]
+    @voucher.exp_date = params[:exp_date]
+    @voucher.save 
+    redirect "/vouchers/#{@voucher.id}"
   end 
 
   delete '/vouchers/:id/delete' do 
-    if logged_in?
-      @voucher = Voucher.find(params[:id])
-      if @voucher.user_id == current_user.id 
-          @voucher.delete 
-      end 
-    end 
+    @voucher = Voucher.find(params[:id]) 
+    @voucher.delete
     redirect '/vouchers'
   end 
+
 end 
