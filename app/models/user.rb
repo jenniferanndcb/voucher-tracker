8401190739d40fd71076f69b32e7385df716b115
Jen <jenniferanndcb@gmail.com>
2019-09-15 01:06:40 +0100

    validates uniqueness login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base 
   has_secure_password
-  validates_presence_of :email, :password 
+  validates :email, presence: true, uniqueness: true 
+  validates_presence_of :password, 
   has_many :vouchers 
   
 end
